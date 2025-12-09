31793ffd61389286c1bbe45c2246aa5dc8c980c5
Breen Young <breen.young@gmail.com>
2025-12-09 16:26:38 -0400

    make route map nested for better navigation

--- app/core/utils.py
+++ app/core/utils.py
@@ -1,15 +1,36 @@
+from fastapi.routing import APIRoute
+
 def get_route_map(app, with_admin_routes: bool = False):
     """
     Generates a dictionary of { "route_name": "/path/{param}" }
     """
     route_map = {}
     for route in app.routes:
-        # We only care about routes that have a name
-        if hasattr(route, "name") and route.name:
+
+        if isinstance(route, APIRoute) and route.name:
 
             tags = getattr(route, "tags", [])
             if not with_admin_routes and "admin" in tags:
                 continue
-            route_map[route.name] = route.path
+
+            # endpoint is guaranteed on APIRoute
+            module_name = getattr(route.endpoint, "__module__", None)
+
+            # prefer tags if present, fall back to module name
+            if tags:
+                prefix = tags[0].replace("-", "_")
+                name = route.name.replace("-", "_")
+            elif module_name:
+                # fallback: use module name as prefix
+                prefix = module_name.replace(".", "_")
+                name = route.name.replace("-", "_")
+            else:
+                prefix = "misc"
+                name = route.name.replace("-", "_")
+
+            # build nested dict
+            if prefix not in route_map:
+                route_map[prefix] = {}
+            route_map[prefix][name] = route.path
 
     return route_map
